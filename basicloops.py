6e408a18054b8a9e28c4d1ef5190c8d2c5dc991b
Candice Garcia <candicegarcia@Candices-MacBook-Air.local>
2020-09-13 19:18:33 -0500

    Basic Loops

---
+++ basicloops.py
@@ -0,0 +1,29 @@
+#Loop through a range of numbers (1 through 20). This will be up to, but not including 21. 
+for x in range(1, 21):
+    print(x)
+
+print("***********************************")
+
+#Iterate through letters in a string: 
+word = "Supercalifragilisticexpialidocious"
+for letters in word:
+    print(letters)
+
+print("***********************************")
+
+#Iterate through a list:
+groceries = ["Milk", "Eggs", "Cheese", "Toilet Paper", "Dish Detergent"]
+print("What did I need from the store again?")
+for grocery in groceries:
+    print(grocery)
+
+print("***********************************")
+
+#Looping with an condition:
+spongebob_reference = "yes"
+
+while spongebob_reference == "yes":
+    print("Wanna see me run to that mountain and back?")
+    spongebob_reference = input("Wanna see me do it again? ")
+
+
